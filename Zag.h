82dc2cacfae1f3a1c0ebb79800b08357b2b206eb
Jap <97182832+JaProCar@users.noreply.github.com>
2025-04-16 15:11:55 +0300

    Update Zag.h
    Перегрузка >><< 1

--- Zag.h
+++ Zag.h
@@ -8,6 +8,8 @@ public:
     string name;
     int sold;
     int used;
+    friend ostream& operator<<(ostream& os, const tickets& Tickets);
+    friend istream& operator>>(istream& is, const tickets& Tickets);
 };
 
 void display();
